001dbf248f2f424b17361d4bd4ab0970a03ec0a6
Chirag-S-kotian <ckotian117@gmail.com>
2024-10-03 10:18:12 +0530

    "Added .env to .gitignore and created thirdweb client in client.ts"

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,1 +1,5 @@
- 
+ import { createThirdwebClient } from "thirdweb";
+
+ const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
+
+ export const client = createThirdwebClient({ clientId: clientId as string });
